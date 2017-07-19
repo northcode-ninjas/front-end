5e56ff8656374d17f5ef3f2f2724b3d73630dc46
ashcode1 <paypertiger@gmail.com>
2017-07-19 12:42:55 +0100

    [feat-26] [Splash] created with name input form

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -1,11 +1,16 @@
 import React from 'react';
+<<<<<<< HEAD
 import { Link } from 'react-router-dom';
 import Sensei1 from '../Images/Sensei1.jpeg';
+=======
+
+>>>>>>> a46933e... [feat-26] [Splash] created with name input form
 
 class Splash extends React.Component {
     render () {
         return (
             <div className="splash">
+<<<<<<< HEAD
                 <img src= {Sensei1} />
                 <div>Welcome Student,<br />please enter your<br />ninja name below:</div>
                 <br />
@@ -13,6 +18,12 @@ class Splash extends React.Component {
                 <form onSubmit="" >
                     <input className="input is-primary" type="text" placeholder="Your ninja name here" />
                     <Link to='/avatar'><button className="button is-primary">Submit</button></Link>
+=======
+                <div>Welcome Student, please enter your ninja name below:</div>
+                <form onSubmit="" >
+                    <input className="input is-primary" type="text" placeholder="Your ninja name here" />
+                    <button className="button is-primary">Submit</button>
+>>>>>>> a46933e... [feat-26] [Splash] created with name input form
                 </form>
             </div>
         )
