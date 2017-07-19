90ce0ff42e75c56ba727243e31d496a338b82f18
ashcode1 <paypertiger@gmail.com>
2017-07-19 11:58:53 +0100

    [feat26] [Question] renders hard-coded questions data

--- src/components/Success.js
+++ src/components/Success.js
@@ -6,7 +6,11 @@ class Success extends React.Component {
     return (
       <div className="Success">
         <h3>Congratulations! You are a code ninja!</h3>
+<<<<<<< HEAD
         <Link to='/question'><button className="button is-primary is-outlined is-large">Continue</button></Link>
+=======
+        <Link to='/question'><button className="button is-primary is-outlined is-large">Submit</button></Link>
+>>>>>>> db87ce2... [feat26] [Question] renders hard-coded questions data
       </div>
     );
   }
