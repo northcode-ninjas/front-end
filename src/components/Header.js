1e975c8358b57ce58fd255d948020455aeeb8412
ashcode1 <paypertiger@gmail.com>
2017-07-20 10:22:30 +0100

    [Header] Karate font installed & [Splash] OpenDyslexic font installed

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,14 @@ import React from 'react';
 class Header extends React.Component {
   render () {
     return (
-      <div className="Header">
-        <h1>Code Ninjas</h1>
-      </div>
+      <section className="hero">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title is-1 big">Code Ninjas</h1>
+          </div>
+        </div>
+      </section>
+      
     );
   }
 }
--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Sensei1 from '../Images/Sensei1.jpeg';
 
 class Splash extends React.Component {
     render () {
         return (
             <div className="splash">
-                <div>Welcome Student, please enter your ninja name below:</div>
+                <img src= {Sensei1} />
+                <div>Welcome Student,<br />please enter your<br />ninja name below:</div>
+                <br />
                 <form onSubmit="" >
                     <input className="input is-primary" type="text" placeholder="Your ninja name here" />
                     <Link to='/avatar'><button className="button is-primary">Submit</button></Link>
