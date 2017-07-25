3a67b07d1ea8163704f665df128d332757c3291c
Ash <paypertiger@gmail.com>
2017-07-25 19:47:55 +0100

    [feat-75] Fully implement LevelUp (#18)

--- src/pages/LevelUp.js
+++ src/pages/LevelUp.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ninjaStar from '../Images/ninja-throwing-star.jpg';
+import axios from 'axios';
+import {ROOT} from '../../config';
+
+const levelNames = ['Academy', 'Gennin', 'Chunnin', 'Jounin', 'Special Jounin', 'Kage', 'ANBU', 'S-Class', 'Ninja'];
+
 
 class LevelUp extends React.Component {
+    componentDidMount() {
+        axios.put (`${ROOT}/users/${this.props.username}/level-up`)
+            .then(() => {
+                this.setState(this.props.questionIndex = 0);
+            });
+        console.log(this.props.questionIndex);
+        // we want to set questionIndex in Level.js to 0 here!!
+        // we also want to increment user.level
+    }
     render () {
         return (
             <div className="levelUp">
                 <img width="50%" src= {ninjaStar} />
-                <span>Well done, student! Your Academy training is complete!</span>
-                <span>Press Graduate to begin Level 2: Genin</span>
-                <Link to='/question'><button className="button is-primary is-outlined is-large"> Graduate</button></Link>
+                <span>Well done, student! Your {levelNames[this.props.level]} training is complete!</span>
+                <span>Press Graduate to begin Level {(+this.props.level) + 2}: {levelNames[+this.props.level + 1]}</span>
+                <Link to={`/levels/${+this.props.level+1}/questions`}><button className="button is-primary is-outlined is-large"> Graduate</button></Link>
                 <Link to='/avatar'><button className="button is-primary is-outlined is-large"> Back</button></Link>
               
             </div>
@@ -18,3 +32,4 @@ class LevelUp extends React.Component {
 }
 
 export default LevelUp;
+
--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -19,7 +19,7 @@ class UserProfile extends React.Component {
                     <div className="UserProfile">
                         <h1 className="title is-1">{user.username}</h1>
                         <h2>Current level: {levelNames[user.level]}</h2>
-                        <Link className="button is-success" to={`/levels/${user.level}`}>Train</Link>
+                        <Link className="button is-success" to={`/levels/${user.level}/questions`}>Train</Link>
                     </div>
                 )}
             </div>
