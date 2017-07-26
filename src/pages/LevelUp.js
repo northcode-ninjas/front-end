1dc22e784403956c3033b73fe65a9a9073d29dbd
janetowen07 <janetowen07@gmail.com>
2017-07-26 13:07:34 +0100

    [feat-82] amend back button (#21)

--- src/pages/LevelUp.js
+++ src/pages/LevelUp.js
@@ -3,19 +3,17 @@ import { Link } from 'react-router-dom';
 import ninjaStar from '../Images/ninja-throwing-star.jpg';
 import axios from 'axios';
 import {ROOT} from '../../config';
+import PropTypes from 'prop-types';
 
 const levelNames = ['Academy', 'Gennin', 'Chunnin', 'Jounin', 'Special Jounin', 'Kage', 'ANBU', 'S-Class', 'Ninja'];
 
-
 class LevelUp extends React.Component {
-    componentDidMount() {
+    componentDidMount () {
         axios.put (`${ROOT}/users/${this.props.username}/level-up`)
             .then(() => {
-                this.setState(this.props.questionIndex = 0);
+                this.setState(this.props.questionIndex = 0)
+                .catch(console.log);
             });
-        console.log(this.props.questionIndex);
-        // we want to set questionIndex in Level.js to 0 here!!
-        // we also want to increment user.level
     }
     render () {
         return (
@@ -23,13 +21,18 @@ class LevelUp extends React.Component {
                 <img width="50%" src= {ninjaStar} />
                 <span>Well done, student! Your {levelNames[this.props.level]} training is complete!</span>
                 <span>Press Graduate to begin Level {(+this.props.level) + 2}: {levelNames[+this.props.level + 1]}</span>
-                <Link to={`/levels/${+this.props.level+1}/questions`}><button className="button is-primary is-outlined is-large"> Graduate</button></Link>
-                <Link to='/avatar'><button className="button is-primary is-outlined is-large"> Back</button></Link>
+                <Link to={`/levels/${+this.props.level + 1}/questions`}><button className="button is-primary is-outlined is-large"> Graduate</button></Link>
+                <Link to={'/profile'}><button className="button is-primary is-outlined is-large"> Restart Training</button></Link>
               
             </div>
         );
     }
 }
 
+LevelUp.propTypes = {
+    username: PropTypes.string.isRequired,
+    questionIndex: PropTypes.number.isRequired,
+    level: PropTypes.string.isRequired
+};
 export default LevelUp;
 
