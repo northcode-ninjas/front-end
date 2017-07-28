fc05f5b91161ab67de1c1277e18f927323af7c42
Janet <janetowen07@gmail.com>
2017-07-28 10:28:58 +0100

    [feat-104][Level-up] adds background for level-up page

--- src/pages/LevelUp.js
+++ src/pages/LevelUp.js
@@ -1,37 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import ninjaStar from '../../public/Images/ninja-throwing-star.jpg';
 import axios from 'axios';
-import {ROOT} from '../../config';
+import { ROOT } from '../../config';
 import PropTypes from 'prop-types';
 
 const levelNames = ['Academy', 'Gennin', 'Chunnin', 'Jounin', 'Special Jounin', 'Kage', 'ANBU', 'S-Class', 'Ninja'];
 
 class LevelUp extends React.Component {
   componentDidMount () {
-  axios.put (`${ROOT}/users/${this.props.username}/level-up`)
-  .then(() => {
-    this.setState(this.props.questionIndex = 0)
-    .catch(console.log);
-    });
+    axios.put(`${ROOT}/users/${this.props.username}/level-up`)
+      .then(() => {
+        this.changeBackground()
+        this.setState(this.props.questionIndex = 0)
+          .catch(console.log);
+  });
+}
+
+changeBackground () {
+    // do the background stuff i.e. grab document and make background image
+    document.getElementsByTagName('body')[0].style.background = 'url("/Images/backgrounds/bird_lake.jpg")' ;
   }
-  render () {
-    return (
-      <div className="levelUp">
-        <span>Well done, student! Your {levelNames[this.props.level]} training is complete!</span>
-        <img width="50%" src= {ninjaStar} />
-        <span>Press Graduate to begin Level {(+this.props.level) + 2}: {levelNames[+this.props.level + 1]}</span>
-        <Link to={`/levels/${+this.props.level + 1}/questions`}><button className="button"> Graduate</button></Link>
-        <Link to={'/profile'}><button className="button"> Restart Training</button></Link>
-      </div>
-    );
-  }
+
+render () {
+  return (
+    <div className="levelUp">
+      <span>Well done, student! Your {levelNames[this.props.level]} training is complete!</span>
+      <span>Press Graduate to begin Level {(+this.props.level) + 2}: {levelNames[+this.props.level + 1]}</span>
+      <Link to={`/levels/${+this.props.level + 1}/questions`}><button className="button"> Graduate</button></Link>
+      <Link to={'/profile'}><button className="button"> Restart Training</button></Link>
+    </div>
+  );
+}
 }
 
 LevelUp.propTypes = {
-    username: PropTypes.string.isRequired,
-    questionIndex: PropTypes.number.isRequired,
-    level: PropTypes.string.isRequired
+  username: PropTypes.string.isRequired,
+  questionIndex: PropTypes.number.isRequired,
+  level: PropTypes.string.isRequired
 };
 export default LevelUp;
 
